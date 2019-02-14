98d61ea2710220c07319ec0585e8094596d13d83
Andres Quiroz <andresq1595@gmail.com>
2019-02-14 14:13:22 -0600

    Activity 02 and beginning of activity03

---
+++ Activity03/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+   Context context;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        context=getApplicationContext();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Notification
+        Notification.Builder notifBuilder=new Notification.Builder(this);
+        notifBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notifBuilder.setContentTitle("This is my shiny notification!");
+        notifBuilder.setContentTitle("This is the detail of the notification");
+        //Toast
+        Toast myToast = new Toast(context);
+        //myToast.makeText(context,"This is a Toast Message",Toast.LENGTH_LONG).show();
+        //Dialog
+        final AlertDialog.Builder DialogConf = new AlertDialog.Builder(this);
+        DialogConf.setTitle("Regular Menus");
+        DialogConf.setMessage("This is the Text of the Dialog");
+        DialogConf.setIcon(R.mipmap.ic_launcher);
+        final EditText TextInput = new EditText(context);
+        DialogConf.setView(TextInput);
+        DialogConf.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        DialogConf.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str = TextInput.getEditableText().toString();
+                Toast.makeText(context,"You wrote this dialog: " + str,Toast.LENGTH_LONG).show();
+
+            }
+        });
+        DialogConf.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        NotificationManager myNotification=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        myNotification.notify(20,notifBuilder.build());
+
+        AlertDialog MyDialog= DialogConf.create();
+        MyDialog.show();
+
+
+
+
+
+    }
+}
