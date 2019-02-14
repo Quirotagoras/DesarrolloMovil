5d92f52aaf80f0e0949fbb9e9d82121826396d20
Andres Quiroz <andresq1595@gmail.com>
2019-02-14 14:16:33 -0600

    Correccion activity01

---
+++ Activity01/app/src/main/java/com/example/activity01/MessageActivity.java
@@ -0,0 +1,20 @@
+package com.example.activity01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        Intent intent = getIntent();
+
+        String message= intent.getStringExtra("EXTRA_MESSAGE");
+        TextView textView = findViewById(R.id.displayMessage);
+        textView.setText(message);
+    }
+}
